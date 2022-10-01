b2e5046cb89ac7f232b79b05933d4bd0247b4d7b
Ashraful Alam <mdmashraful1998@gmail.com>
2022-10-01 17:39:00 +0600

    install redux and declare initial state

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+import DISHES from '../data/Dishes';
+import COMMENTS from '../data/Comments';
+
+const initialState = {
+    dishes: DISHES,
+    comments: COMMENTS
+}
+
+
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+}
